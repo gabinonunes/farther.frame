8a710403557920fd3064c8aba33c732747b5879f
Gabino Nunes <gabinonunes@gmail.com>
2024-08-13 18:42:52 -0400

    new

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -33,17 +33,33 @@ const handleRequest = frames(async (ctx) => {
       <div style={{
         backgroundColor: 'black',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
         height: '100%'
       }}>
-        <img
-          src={`${baseUrl}/farther.png`}
-          alt="Farther"
-          width="300"
-          height="300"
-        />
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginBottom: '20px'
+        }}>
+          <img
+            src={`https://uc9ba1bc4c0d397bff6ccf001827.previews.dropboxusercontent.com/p/thumb/ACXPe9atf7nFFy10QDqC-bDFiSkQCnyV3HGjEFypzh2vMERx1nCVO703l3HwV0De5bskVbtjvHcClZQrejV9AFrCfTg2ZvQ8nHJC5rKpnnTf3EJq-HNbSb3oPlLvaSxofe8q6BCQJXi7JWi77mvF20s7ZgsZNPwmHUQoyoLlAFHvvU5hi5FAVOAYlqVYAqYarqSuLUery-FXRdneYhpsk-2ehXJ44LoywHgpDAw8ZVcKd-6fg7C6txnGGw_OdRPqfSS0AAs1yptMMB13FLiBEH3SC2v-8zlK6CvEiyVmnyO4Qxnw_uj94QJ_Bryc8f2LZiOy7B5m2EWHP13bM--XiAmc/p.png`}
+            alt="Farther"
+            width="200"
+            height="200"
+          />
+        </div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          textAlign: 'center'
+        }}>
+          <h1 style={{ color: 'white', fontSize: '80px', fontWeight: 'bold' }}>Check Your Farther Allowance</h1>
+        </div>
       </div>
     ),
     
@@ -51,9 +67,7 @@ const handleRequest = frames(async (ctx) => {
       <Button action="post" target='/frames/route1'>
         My Stats
       </Button>,
-      <Button action="post" target='/route2'>
-        Share
-      </Button>,
+      
     ],
   };
 });
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import { fetchMetadata } from "frames.js/next";
- 
+
 export async function generateMetadata() {
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000';
+
+  const framesMetadata = await fetchMetadata(new URL('/frames', baseUrl));
+
   return {
     title: "Farther Allowance Tracker",
-    // provide a full URL to your /frames endpoint
-    other: await fetchMetadata(
-      new URL(
-        "/frames",
-        process.env.NEXT_PUBLIC_VERCEL_URL
-          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-          : "http://localhost:3000"
-      )
-    ),
+    other: framesMetadata,
   };
 }
- 
+
 export default function Page() {
   return <span>Farther Allowance Tracker</span>;
 }

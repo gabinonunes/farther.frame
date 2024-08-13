dcbd26b0c605f84820c79549cb2e6b82f699051b
Gabino Nunes <gabinonunes@gmail.com>
2024-08-12 21:52:11 -0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,28 @@
 import { fetchMetadata } from "frames.js/next";
- 
-export async function generateMetadata() {
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const frameMetadata = await fetchMetadata(
+    new URL(
+      "/frames",
+      process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000"
+    )
+  );
+
   return {
-    title: "My Page",
-    // provide a full URL to your /frames endpoint
-    other: await fetchMetadata(
-      new URL(
-        "/frames",
-        process.env.VERCEL_URL
-          ? `https://${process.env.VERCEL_URL}`
-          : "http://localhost:3000"
-      )
-    ),
+    title: "Farther Allowance Tracker",
+    description: "Farther Allowance Tracker",
+    other: {
+      "fc:frame": "vNext",
+      "fc:frame:image": `https://farther.social/_next/image?url=%2Fimages%2Flanding-page-placeholder.png&w=3840&q=75`,
+      "og:image": `https://farther.social/_next/image?url=%2Fimages%2Flanding-page-placeholder.png&w=3840&q=75`,
+    },
+    ...frameMetadata,
   };
 }
  
 export default function Page() {
-  return <span>My existing page</span>;
+  return <span>Farther Allowance Tracker</span>;
 }
